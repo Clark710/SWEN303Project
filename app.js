1e010e89f2ddac25d722014aa084a8be3182b377
Clark710 <neel.pat007@gmail.com>
2016-03-27 01:45:11 +1300

    Additional file Added in new browse page link to navbar

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var explore = require('./routes/explore');
+var search = require('./routes/search');
+var browse = require('./routes/browse');
 
 var app = express();
 
@@ -25,7 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/explore', explore);
+app.use('/search', search);
+app.use('/browse', browse);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
