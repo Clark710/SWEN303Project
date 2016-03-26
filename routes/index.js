242ddec87d1422d9cf777a06af1b13f80d3a59e3
Clark710 <neel.pat007@gmail.com>
2016-03-27 02:01:22 +1300

    Functionality added The webiste can now perform basic string searches, lists all xml files in the colenso datbase and also can display each xml file for the user to view.

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,56 @@ var client = new basex.Session("127.0.0.1", 1984, "admin", "admin");
 client.execute("OPEN Colenso");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Colenso Project' });
+});
+
+/* GET search page. */
+router.get('/search', function(req, res) {
+    var query = "XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" +
+        "for $t in (//title[. contains text '" + req.query.searchString + "'])\n" +
+        "return concat('<a href=\"/file?filename=', db:path($t), '\" class=\"searchResult\">', $t, '</a>'," +
+        "'<p class=\"searchResult\">', db:path($t), '</p>')";
+
+    client.execute(query,
+        function (error, result) {
+            if(error) {
+                console.error(error);
+            }
+            else {
+                var nResults = (result.result.match(/<\/a>/g) || []).length;
+                res.render('search', { title: 'Colenso Project', results: result.result, nResults: nResults});
+            }
+        }
+    );
+});
+
+/* GET full list of xml files. */
+router.get("/browse",function(req,res){
+    client.execute("XQUERY db:list('Colenso')",
+        function (error, result) {
+            if(error){ console.error(error);}
+            else {
+                var splitlist = result.result.split("\n")
+                res.render('browse', { title: 'Colenso Project', place: splitlist });
+            }
+        }
+    );
+});
+
+/* GET xml file from database. */
+router.get('/file', function(req, res) {
+    var query = "XQUERY doc('Colenso/" + req.query.filename + "')";
+    client.execute(query,
+        function (error, result) {
+            if(error) {
+                console.error(error);
+            }
+            else {
+                res.render('file', { title: 'Colenso Project', data: result.result });
+            }
+        }
+    );
 });
 
 module.exports = router;
