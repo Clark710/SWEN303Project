b86d5410f0a421ea75dca0c042ed1fa79ca8636d
Clark710 <neel.pat007@gmail.com>
2016-03-28 11:44:44 +1300

    Delete browse.js

--- routes/browse.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var basex = require('basex');
-var client = new basex.Session("127.0.0.1", 1984, "admin", "admin");
-client.execute("OPEN Colenso");
-
-module.exports = router;
