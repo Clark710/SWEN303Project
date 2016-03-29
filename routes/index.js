fb244fe99826e024b66206ee81ceca99c81f32b8
Clark710 <neel.pat007@gmail.com>
2016-03-30 00:50:16 +1300

    Changes made to file: - text search function can now search using AND and OR operations - can now also search using xquery/xpath -changes made to browse query as list of all files is in a new and better designed structure -attemped to add ability to view file in raw xml (still needs development)

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var basex = require('basex');
+var multer = require('multer');
 var client = new basex.Session("127.0.0.1", 1984, "admin", "admin");
 client.execute("OPEN Colenso");
 
@@ -11,18 +12,57 @@ router.get('/', function(req, res) {
 
 /* GET search page. */
 router.get('/search', function(req, res) {
+    if(req.query.searchString){
+        var array = req.query.searchString.split(" ");
+        var queryString = "";
+        var i = 0;
+        while(i < array.length){
+            queryString += array[i];
+            ++i;
+            if(i < array.length){
+                if(array[i] === "AND"){
+                    queryString += "' ftand '";
+                }else if(array[i] === "OR"){
+                    queryString += "' ftor '";
+                }else if(array[i] === "NOT"){
+                    queryString += "' ftnot '";
+                }
+            }
+            ++i;
+        }
+    }
     var query = "XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" +
-        "for $t in (collection('Colenso')[. contains text '" + req.query.searchString + "'])\n" +
-        "return concat('<a href=\"/file?filename=', db:path($t), '\" class=\"searchResult\">', $t, '</a>'," +
-        "'<p class=\"searchResult\">', db:path($t), '</p>')";
-
+        "for $n in (//title[. contains text ' "  + queryString + "'])\n" +
+        " return concat('<a href=\"/file?filename=', db:path($n), '\" class=\"searchResult\">', $n, '</a>'," +
+        "'<p class=\"searchResult\">', db:path($n), '</p>')";
     client.execute(query,
         function (error, result) {
             if(error) {
                 console.error(error);
             }
             else {
-                res.render('search', { title: 'Colenso Project', results: result.result});
+                var nResults = (result.result.match(/<\/a>/g) || []).length;
+                res.render('search', { title: 'Colenso Project', results: result.result, nResults: nResults});
+            }
+        }
+    );
+});
+
+/* GET search page. */
+router.get('/xquery', function(req, res) {
+    var xquery = "XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" +
+        "for $n in " + req.query.searchString +
+        " return concat('<a href=\"/file?filename=', db:path($n), '\" class=\"searchResult\">', $n, '</a>'," +
+        "'<p class=\"searchResult\">', db:path($n), '</p>')";
+
+    client.execute(xquery,
+        function (error, result) {
+            if(error) {
+                console.error(error);
+            }
+            else {
+                var nResults = (result.result.match(/<\/a>/g) || []).length;
+                res.render('xquery', { title: 'Colenso Project', results: result.result, nResults: nResults});
             }
         }
     );
@@ -30,11 +70,16 @@ router.get('/search', function(req, res) {
 
 /* GET full list of xml files. */
 router.get("/browse",function(req,res){
-    client.execute("XQUERY db:list('Colenso')",
+    var query = "XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" +
+        "for $n in (//title)\n" +
+        "where db:path($n) contains text '" + req.query.type + "'" +
+        "return concat('<a href=\"/file?filename=', db:path($n), '\" class=\"searchResult\">', $n, '</a>'," +
+        "'<p class=\"searchResult\">', db:path($n), '</p>')";
+    client.execute(query,
         function (error, result) {
             if(error){ console.error(error);}
             else {
-                var splitlist = result.result.split("\n")
+                var splitlist = result.result.split("\n");
                 res.render('browse', { title: 'Colenso Project', place: splitlist });
             }
         }
@@ -50,7 +95,27 @@ router.get('/file', function(req, res) {
                 console.error(error);
             }
             else {
-                res.render('file', { title: 'Colenso Project', data: result.result });
+                var fileName = result.result.match(/<title>[\s\S]*?<\/title>/)[0];
+                fileName = fileName.replace("<title>", "");
+                fileName = fileName.replace("</title>", "");
+                res.render('file', { title: 'Colenso Project', fileName: fileName, data: result.result });
+            }
+        }
+    );
+});
+
+/* GET xml file from database. */
+router.get('/rawFile', function(req, res) {
+    var query = "XQUERY doc('Colenso/" + req.query.filename + "')";
+    client.execute(query,
+        function (error, result) {
+            if(error) {
+                console.error(error);
+            }
+            else {
+                var url_list = req.url.split('/');
+                var url = req.url.replace("/" + url_list[0], url_list[0]);
+                res.render('rawFile', { title: 'Colenso Project', data: result.result, url: url });
             }
         }
     );
